c722ec025f3e3f922338f1d179344ef687260aed
rsalgo <tinisufina@gmail.com>
2024-04-29 10:34:36 +0100

    requirements.txt from venv

--- simpleserver.py
+++ simpleserver.py
@@ -1,3 +1,14 @@
+# python -m venv venv
+# VM On Linux: source ./venv/bin/activate
+# run python simpleserver.py > it will say No module named 'flask'
+# so pip install flask, then rerun python simpleserver.py
+# http://127.0.0.1:5000 > hello from simple server
+# Crtl + C
+# pip freeze > requirement.txt > ls > (requirements.txt will be there)
+# git commit to github
+
+
+
 from flask import Flask
 
 app = Flask(__name__, static_url_path='', static_folder='static')
